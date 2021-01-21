2f5216f3256825a2112409966847896251b0f769
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-21 12:52:16 -0500

    commit tree 3b322dac57551f7f62e42ce65bf1ac25eb5da339

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,16 +9,18 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+
+    3.times { @song.notes.build }
   end
 
   def create
-    @song = Song.new(song_params)
+    Song.create(song_params)
 
-    if @song.save
-      redirect_to @song
-    else
-      render :new
-    end
+    # if @song.save
+    #   redirect_to @song
+    # else
+    #   render :new
+    # end
   end
 
   def edit
@@ -47,7 +49,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:artist_name, :title, :genre_id, :note_contents => [])
   end
 end
 
